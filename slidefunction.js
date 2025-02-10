db4918454cc83e2f7abe48bf6b62c7dd43c121bb
ppauloa37 <ppauloa37@gmail.com>
2025-02-10 13:32:37 -0300

    Update slidefunction.js

--- slidefunction.js
+++ slidefunction.js
@@ -15,6 +15,13 @@ hello word
 |
 structure site
 */
+    this.images = [];
+    this.curImage = 0;
+    for (i = 0; i < container.childElementCount; i++) {
+        this.images.push(container.children[i]);
+        this.images[i].style.display = "none";
+    }
+
     var nextSlide = function() {
         for (var i = 0; i < this.images.length; i++) {
             this.images[i].style.display = "none";
@@ -24,9 +31,9 @@ structure site
         if (this.curImage >= this.images.length) {
             this.curImage = 0;
         }
-        window.setTimeout(nextSlide.bind(this), 1000);
+        window.setTimeout(nextSlide.bind(this), 2000);
     };
     
     nextSlide.call(this);
 };
-slideShow(document.getElementById("slideshow"));
+
